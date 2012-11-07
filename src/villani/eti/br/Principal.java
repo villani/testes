91870c6471cf409e4c9e0af1f18ec284225b8329
Leonardo Villani <informatica@villani.eti.br>
2012-11-07 15:07:29 -0200

    Aplicação simples usando Swing.

---
+++ src/villani/eti/br/Principal.java
@@ -0,0 +1,17 @@
+package villani.eti.br;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = JOptionPane.showInputDialog("Informe seu nome:");
+		JOptionPane.showMessageDialog(null, "Olá, " + nome);
+
+	}
+
+}
